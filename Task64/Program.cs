87c8e79404f989ad74f56aee01d786ede5aa9d57
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-05 13:37:43 +0300

    Task64 done

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+string f(int n)
+{
+  if (n== 1)
+    return $"{n}";
+  return $"{n}, "+f(n-1);
+}
+
+Console.Clear();
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
+
