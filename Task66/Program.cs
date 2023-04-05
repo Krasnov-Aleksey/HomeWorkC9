82847d98aa8fdfc2eda1f7bc92fc6c2ad445e5f6
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-05 13:54:54 +0300

    Task66 done

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int f(int m, int n)
+{
+  if (m==n)
+    return m;
+  return f(m,n-1)+n;
+}
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m,n));
