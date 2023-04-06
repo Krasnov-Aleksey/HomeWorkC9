2b5712a2f1a34938db0fabb9c454e1581c7653e7
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-06 09:49:19 +0300

    Task 68 done

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//функции Аккермана
+
+int f(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return f(m - 1, 1);
+    }
+    else
+    {
+        return f(m - 1, f(m, n - 1));
+    }
+}
+
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
+
+
